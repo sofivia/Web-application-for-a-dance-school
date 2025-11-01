19dd979c984f764eb1b0a20f53e0ecb5114d0b43
Wojciech Kukiełka <wojciech.kukielka.stud@pw.edu.pl>
2025-11-01 17:58:39 +0100

    create database app user

---
+++ db/setup/01-init.js
@@ -0,0 +1,16 @@
+'use strict';
+(() => {
+    const testdb = db.getSiblingDB("test");
+    testdb.dropDatabase();
+
+    const appPasswd = fs.readFileSync("/run/secrets/db_app_passwd", "utf8");
+    db.createUser({
+        user: "app",
+        pwd: appPasswd,
+        roles: [{ role: "readWrite", db: "szkola" }]
+    });
+
+    db.getCollection("students").insertMany([
+        { "name": "kij", age: 12 },
+        { "name": "piłka", age: 21 }]);
+})()
