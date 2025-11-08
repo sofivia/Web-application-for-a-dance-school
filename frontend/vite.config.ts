a182b2b60f69d2106bcc4fb5aea803d48feee498
wkukielk <wojciech.kukielka.stud@pw.edu.pl>
2025-11-08 16:05:54 +0100

    config vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
+
 })
---
+++ genscript.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+gen_script() {
+    JOBNAME="$1"
+    AFTERJOB=`cat .gitlab-ci.yml | sed -n "/$JOBNAME/,"'$p'`
+    AFTERSCRIPT=`echo "$AFTERJOB" | sed -n "/script/,"'$p'  | sed '1d'`
+    SCRIPT=`echo "$AFTERSCRIPT" | awk '!/[[:space:]]-/ {exit} 1'`
+    ONELINE=`echo "$SCRIPT" | tr '\n' ' '`
+    DASHSPLIT=`echo $ONELINE | sed -E 's/-[[:space:]]/\n/g' | sed '1d'`
+    echo "$DASHSPLIT"
+    echo "$DASHSPLIT" | while read line; do
+        echo -n "$line; "
+    done
+}
+
+read_image() {
+    JOBNAME="$1"
+    AFTERJOB=`cat .gitlab-ci.yml | sed -n "/$JOBNAME/,"'$p'`
+    IMAGELINE=`echo "$AFTERJOB" | sed -n '/image/p' | sed -n '1p'`
+    echo $IMAGELINE | (read _ image; echo "$image" )
+}
+
+JOBNAME="$1"
+IMAGE=`read_image "$JOBNAME"`
+SCRIPT=`gen_script "$JOBNAME"`
+SHELL=`test -n "$2" && echo "$2" || echo "sh"`
+
+echo "$SCRIPT"
