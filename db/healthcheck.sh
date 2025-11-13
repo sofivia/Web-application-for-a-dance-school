9cd2351f7f9ca8e18e2d0394f7a976a8e195a266
wkukielk <wojciech.kukielka.stud@pw.edu.pl>
2025-11-13 14:03:24 +0100

    enable tls in db

--- db/healthcheck.sh
+++ db/healthcheck.sh
@@ -2,5 +2,6 @@
 
 PING_PASSWD=`cat /run/secrets/ping_passwd`
 CMD="db.runCommand('ping').ok"
-test `mongosh "mongodb://ping:${PING_PASSWD}@localhost/admin" --eval "$CMD" --quiet` \
+OPTS="--tls --tlsAllowInvalidCertificates --quiet"
+test `mongosh "mongodb://ping:${PING_PASSWD}@localhost/admin" $OPTS --eval "$CMD"` \
     -eq 1
