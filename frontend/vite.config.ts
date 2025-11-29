fb875c21f2da05467cdf859366f447da0cc14122
wkukielk <wojciech.kukielka.stud@pw.edu.pl>
2025-11-30 00:00:13 +0100

    install svgr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    svgr()
   ],
   resolve: {
     alias: {
