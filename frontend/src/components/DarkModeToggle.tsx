74ef399aa1ef4e04c3c5bc42b1bca8525e1e66fc
wkukielk <wojciech.kukielka.stud@pw.edu.pl>
2025-12-17 13:57:08 +0100

    fix small screen issue

--- frontend/src/components/DarkModeToggle.tsx
+++ frontend/src/components/DarkModeToggle.tsx
@@ -8,7 +8,7 @@ export default function DarkModeToggle() {
     const toggleId = useId();
 
     return (<div>
-        <label htmlFor={toggleId} className='pr-2'> Tryb ciemny </label>
+        <label htmlFor={toggleId} className='text-nowrap pr-2 mb-1'> Tryb ciemny </label>
         <Toggle callback={setActive} isOn={theme == 'dark'} id={toggleId} />
     </div>)
 }
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -9,40 +9,34 @@ export default function Navigation() {
    const { isLoggedIn } = useAuth();
    return (
       <div className={`${styles.navbar}`}>
-         <div className={styles.left}>
-            <Link to="/">
-               <Logo className={`${styles.logo} mb-3`} aria-label="Logo TipTap" />
-            </Link>
+         <Link to="/" className="mr-2">
+            <Logo className={`${styles.logo}`} aria-label="Logo TipTap" />
+         </Link>
 
-            {isLoggedIn && (
-               <nav className={styles.nav}>
-                  <NavLink to="/attendance" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                     Dziennik obecności
-                  </NavLink>
+         {isLoggedIn && (
+            <nav className={styles.nav}>
+               <NavLink to="/attendance" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
+                  Dziennik obecności
+               </NavLink>
 
-                  <NavLink to="/payments" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                     Płatności
-                  </NavLink>
+               <NavLink to="/payments" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
+                  Płatności
+               </NavLink>
 
-                  <NavLink to="/classReg" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                     Zapis na zajęcia
-                  </NavLink>
-               </nav>
-            )}
-         </div>
+               <NavLink to="/classReg" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
+                  Zapis&nbsp;na zajęcia
+               </NavLink>
+            </nav>
+         )}
 
          <div className={styles.right}>
             <DarkModeToggle />
 
-            {!isLoggedIn ? (
-               <></>
-            ) : (
-               <NavLink to="me/" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                  <div>
-                     <UserButton />
-                  </div>
-               </NavLink>
-            )}
+            {isLoggedIn && <NavLink to="me/" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
+               <div>
+                  <UserButton />
+               </div>
+            </NavLink>}
          </div>
       </div>
    );
