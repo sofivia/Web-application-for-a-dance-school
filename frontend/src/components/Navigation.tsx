692efa77496294e01d2d5b2fb8b7ddaccc514811
wkukielk <wojciech.kukielka.stud@pw.edu.pl>
2025-12-19 20:22:36 +0100

    navigation has different content for users of different roles

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,42 +1,50 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, type NavLinkRenderProps } from "react-router-dom";
 import DarkModeToggle from "@/components/DarkModeToggle";
 import styles from "./Navigation.module.css";
 import UserButton from "./UserButton";
 import Logo from "@/assets/tip-tap-logo.svg?react";
 import { useAuth } from "@/utils/auth/useAuth";
+import type { Role } from "@/api";
+
+type NamedLink = [string, string];
+
+const links: Record<Role, NamedLink[]> = {
+   student: [
+      ["Dziennik obecności", "/attendance"],
+      ["Płatności", "/payments"],
+      ["Zapisy na zajęcia", "/classReg"]],
+   instructor: [
+      ["Grupy zajęciowe", "/classes"]
+   ],
+   admin: [
+      ["Płatności", "/studPayments"]
+   ]
+};
 
 export default function Navigation() {
-   const { isLoggedIn } = useAuth();
+   const { isLoggedIn, role } = useAuth();
+   const styl = ({ isActive }: NavLinkRenderProps) => (isActive ? styles.active : styles.link);
    return (
       <div className={`${styles.navbar}`}>
          <Link to="/" className="mr-2">
             <Logo className={`${styles.logo}`} aria-label="Logo TipTap" />
          </Link>
 
-         {isLoggedIn && (
+         {isLoggedIn &&
             <nav className={styles.nav}>
-               <NavLink to="/attendance" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                  Dziennik obecności
-               </NavLink>
-
-               <NavLink to="/payments" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                  Płatności
-               </NavLink>
-
-               <NavLink to="/classReg" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-                  Zapis&nbsp;na zajęcia
-               </NavLink>
+               {role == undefined ? <></> : links[role].map(link => <NavLink to={link[1]} className={styl}>
+                  {link[0]}
+               </NavLink>)}
             </nav>
-         )}
+         }
 
          <div className={styles.right}>
             <DarkModeToggle />
 
-            {isLoggedIn && <NavLink to="me/" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
-               <div>
+            {isLoggedIn &&
+               <NavLink to="me/" className={({ isActive }) => (isActive ? styles.active : styles.link)}>
                   <UserButton />
-               </div>
-            </NavLink>}
+               </NavLink>}
          </div>
       </div>
    );
