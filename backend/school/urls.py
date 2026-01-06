0acb14cc95c4fbe508637055218d6811a3698b0f
vik-4994 <igor.gavrylenko05@gmail.com>
2026-01-06 16:30:14 +0100

    Added backend for classes ui

--- backend/school/urls.py
+++ backend/school/urls.py
@@ -11,7 +11,8 @@ from .views import (
     ClassGroupView,
     EnrollView,
     UnenrollView,
-    AccountViewSet
+    AccountViewSet,
+    ClassSessionParticipantsView
 )
 
 app_name = "school"
@@ -28,6 +29,9 @@ urlpatterns = [
     path("classes/", ClassSessionListView.as_view(), name="classes"),
     path("enroll/", EnrollView.as_view(), name="enroll"),
     path("unenroll/", UnenrollView.as_view(), name="unenroll"),
+    path("classes/", ClassSessionListView.as_view(), name="classes"),
+    path("classes/<str:session_id>/participants/", ClassSessionParticipantsView.as_view(),
+         name="class_session_participants"),
 ]
 
 urlpatterns += router.urls
